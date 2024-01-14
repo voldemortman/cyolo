37701163438e80d13df7009b2908e3c1c51a69b9
Daniel Mortman <danielmortman@gmail.com>
2024-01-14 13:37:13 +0200

    Change the controller to work with the servicef

--- README.md
+++ README.md
@@ -76,7 +76,10 @@ Nest is [MIT licensed](LICENSE).
 ## TODO
 - Add an interceptor that turns the comma seperated values to a list of strings
 - Add an interceptor that turns the stats into an enum
-
+- Add an sqljs db
+- Error handling for stats if stat type doesn't exist
+- Do I want to store a sorted array? How would that be scaleable?
+- Maybe get least should be called get least frequency cause it's confusing.
 
 ## Questions
 - If there are an even amount of words, do I want the average of the middle two or just pick one?
--- src/words/words.controller.ts
+++ src/words/words.controller.ts
@@ -1,14 +1,24 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { WordsService } from './words.service';
 
 @Controller('words')
 export class WordsController {
+  private statsActions: {
+    [key: string]: () => number | { [key: string]: number }[];
+  } = {};
+  constructor(private wordsService: WordsService) {
+    this.statsActions['top'] = this.wordsService.getTopFive;
+    this.statsActions['median'] = this.wordsService.getMedian;
+    this.statsActions['least'] = this.wordsService.getLeast;
+  }
+
   @Post()
   async PostWords(@Body() words: string[]) {
-    return `Unimplemented ${words}`;
+    this.wordsService.addWords(words);
   }
 
   @Get()
   async GetStats(@Body() statsType: string) {
-    return `Unimplemented ${statsType}`;
+    return this.statsActions[statsType]();
   }
 }
